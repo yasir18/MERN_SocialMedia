ec176b3a1c20415fef34f4d1afbcae2816e8e3c1
Yasir Affan <yasiraffan9@gmail.com>
2021-01-13 23:53:03 +0530

    preparing for deployment

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ const profileRouter = require('./routes/profileRoutes');
 const postRouter = require('./routes/postRoutes');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-	console.log('Server running on port ' + PORT);
-});
-
 connectDb();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('uploads/'));
 
-app.use('/', defaultRouter);
+// app.use('/', defaultRouter);
 app.use('/api/user', userRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/posts/', postRouter);
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log('Server running on port ' + PORT);
+});
